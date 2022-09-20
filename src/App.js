e4e52bcfd3795155630d9f8ab5e897aed0da0612
Tural Musaibov <tural.musaibov@gmail.com>
2022-09-20 07:17:25 +0400

    initialCards object is parsed from images directory

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, {useState} from "react";
 import CardBoard from "./components/CardBoard/CardBoard";
-import neptune from "./neptune.png"
 
-const initialCards = [
-   {
-      'name':'neptune',
-      'image': neptune
-   },
-]
+const imageSources = {};
 
+function importAll(r) {
+  r.keys().forEach((key) => (imageSources[key] = r(key)));
+}
 
+importAll(require.context('./images/', false, /\.(png|jpe?g|svg)$/));
 
+const initialCards = Object.entries(imageSources)
+.map(([key, value]) => ({
+   'name': key.match(/\w+/)[0],
+   'image': value
+}));
 
 
 const App = () => {
